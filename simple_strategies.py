a6c0fbf113c457875c5111c8b797e67e0667e289
twhalen <thor@otosense.com>
2019-07-24 17:32:34 -0700

    added doctests to sma_strategies

--- simple_strategies.py
+++ simple_strategies.py
@@ -6,7 +6,7 @@ def _ensure_series(x):
     if isinstance(x, pd.DataFrame):
         return x[x.columns[0]]
     elif not isinstance(x, pd.Series):
-        return pd.Series(x)
+        return pd.Series(x, name='val')
     else:
         return x
 
@@ -23,6 +23,26 @@ def sma_strategies(series, short_wins=range(3, 7 * 3), long_wins=range(7, 7 * 6,
 
     Yields:
         Dicts with win sizes, market and stragegy perf, position arrays nad some stats
+
+    >>> from pprint import pprint
+    >>> stock_val_series = [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 3, 4, 5, 6, 7, 8]
+    >>> it = sma_strategies(stock_val_series, [1, 2], [3, 4, 5])
+    >>> pprint(next(it))
+    {'lift': 4.740740740740738,
+     'long_win': 3,
+     'market_perf': 2.666666666666666,
+     'n_position_switches': 2,
+     'position': array([ 1,  1,  1, -1, -1, -1, -1,  1,  1,  1,  1,  1,  1]),
+     'short_win': 1,
+     'strategy_perf': 7.407407407407404}
+    >>> pprint(next(it))
+    {'lift': 1.125,
+     'long_win': 4,
+     'market_perf': 2.0,
+     'n_position_switches': 2,
+     'position': array([ 1,  1, -1, -1, -1, -1, -1,  1,  1,  1,  1,  1]),
+     'short_win': 1,
+     'strategy_perf': 3.125}
     """
     series = _ensure_series(series)
     val_name = series.name
